9b5acab5a7e8beb456db910fdaebf80f2644d522
Janice Lee <janicelee1120@gmail.com>
2018-01-20 01:11:27 -0800

    Edits and experimenting

---
+++ js/testing.js
@@ -0,0 +1,7 @@
+alert("Hello World!");
+const huh = window.document.getElementById("lol");
+//console.log(huh.innerText);
+// huh.innerText = "POOP";
+// console.log("what");
+
+var huhs = $("#huh");
